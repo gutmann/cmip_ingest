5bbedbddfcbb011ed35fe100a37512560d2d0963
Ethan Gutmann <gutmann@ucar.edu>
2019-03-13 12:49:26 -0700

    Use CDO to regrid ssts to the atmospheric grid

--- scripts/icar/io_routines.py
+++ scripts/icar/io_routines.py
@@ -5,6 +5,12 @@ import netCDF4
 from bunch import Bunch
 import mygis
 
+from cdo import Cdo
+
+global cdo
+cdo = None # don't initialize the climate data operators if they aren't going to be used.
+
+
 g=9.8
 atmvarlist=["ta","hus","ua","va"]
 icar_atm_var=["t","qv","u","v"]
@@ -17,9 +23,9 @@ def read_nc(filename,var="data",proj=None,returnNCvar=False):
         data:raw data as an array
         proj:string representation of the projection information
         atts:data attribute dictionary (if any)
-    if (returnNCvar==True) then the netCDF4 file is note closed and the netCDF4 
-        representation of the variable is returned instead of being read into 
-        memory immediately.  
+    if (returnNCvar==True) then the netCDF4 file is note closed and the netCDF4
+        representation of the variable is returned instead of being read into
+        memory immediately.
     '''
     d=netCDF4.Dataset(filename, mode='r',format="nc")
     outputdata=None
@@ -39,8 +45,8 @@ def read_nc(filename,var="data",proj=None,returnNCvar=False):
     if proj!=None:
         projection=d.variables[proj]
         outputproj=str(projection)
-    
-    
+
+
     if returnNCvar:
         return Bunch(data=outputdata,proj=outputproj,ncfile=d,atts=attributes)
     d.close()
@@ -53,7 +59,7 @@ def find_atm_file(time,varname,info):
     file_base= file_base.replace("_Y_",str(time.year))
     file_base= file_base.replace("_EXP_",info.experiment)
     atm_file = file_base.replace("_ENS_",info.ensemble)
-    
+
     print(atm_file)
     filelist = glob.glob(atm_file)
     filelist.sort()
@@ -62,12 +68,12 @@ def find_atm_file(time,varname,info):
 def find_sst_file(time,info):
     file_base= info.atmdir+info.atmfile
     file_base= file_base.replace("_GCM_",info.gcm_name)
-    file_base= file_base.replace("_VAR_","sst")
+    file_base= file_base.replace("_VAR_","tos")
     file_base= file_base.replace("6hrLev","day")
     file_base= file_base.replace("_Y_",str(time.year))
     file_base= file_base.replace("_EXP_",info.experiment)
     sst_file = file_base.replace("_ENS_",info.ensemble)
-    
+
     print(sst_file)
     filelist=glob.glob(sst_file)
     filelist.sort()
@@ -76,7 +82,7 @@ def find_sst_file(time,info):
 
 def load_atm(time,info):
     """Load atmospheric variable from a netcdf file"""
-    
+
     outputdata=Bunch()
 
     for s,v in zip(icar_atm_var,atmvarlist):
@@ -98,24 +104,24 @@ def load_atm(time,info):
             outputdata[varname]=np.concatenate([outputdata[varname],newdata])
         else:
             outputdata[varname]=newdata
-        
+
         varname="p"
         newdata=info.read_pressure(atmfile)[:,:,info.ymin:info.ymax,info.xmin:info.xmax]
         if varname in outputdata:
             outputdata[varname]=np.concatenate([outputdata[varname],newdata])
         else:
             outputdata[varname]=newdata
-            
+
         outputdata.ntimes = outputdata.p.shape[0]
-        
+
     # outputdata.times=info.read_time(atmfile)
     try:
         calendar = mygis.read_attr(atmfile_list[0], "calendar", varname="time")
     except KeyError,IndexError:
         calendar = None
-    
+
     outputdata.calendar = calendar
-    
+
     return outputdata
 
 def load_sfc(time,info):
@@ -123,8 +129,21 @@ def load_sfc(time,info):
     outputdata=Bunch()
     basefile=info.orog_file
     outputdata.hgt=read_nc(basefile,"orog").data[info.ymin:info.ymax,info.xmin:info.xmax]
-    # sstfile=find_sst_file(time)
-    # outputdata.sst=read_nc(sstfile,"sst").data[:,info.ymin:info.ymax,info.xmin:info.xmax]
+
+    sstfile, timeidx = find_sst_file(time)
+    # cdo griddes va_6hrLev_CCSM4_historical_r6i1p1_1980100100-1980123118.nc  > grid.txt
+    # subprocess.call(["cdo","griddes","{gridfile}  > grid.txt".format(gridfile=basefile)])
+    # cdo -f nc -s remapcon,grid.txt tos_day_CCSM4_historical_r6i1p1_19700101-19891231.nc tos2_day_CCSM4_historical_r6i1p1_19700101-19891231.nc
+    # subprocess.call(["cdo","-f nc", "-s remapcon,grid.txt {input_sst} {regridded_sst}".format(input_sst=sstfile, regridded_sst="regridded_"+sstfile)])
+
+    global cdo
+    if cdo is None:
+        cdo=Cdo()
+    cdo.griddes("-f "+basefile+" >grid.txt")
+    print("regridding:"+sstfile)
+    cdo.remapcon("grid.txt ", input=sstfile, output="regridded_"+sstfile, options="-f nc")
+    sstfile = "regridded_"+sstfile
+    outputdata.sst=read_nc(sstfile,"tos").data[timeidx,info.ymin:info.ymax,info.xmin:info.xmax]
 
     outputdata.land=np.zeros(outputdata.hgt.shape)
     landfrac=read_nc(basefile,"sftlf").data[info.ymin:info.ymax,info.xmin:info.xmax]
@@ -137,5 +156,3 @@ def load_data(time,info):
     atm=load_atm(time,info)
     sfc=load_sfc(time,info)
     return Bunch(sfc=sfc,atm=atm)
-
-
