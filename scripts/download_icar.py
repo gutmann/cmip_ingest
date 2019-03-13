860e146835fcf8dffa979e8360c43b33f74e3f53
Ethan Gutmann <gutmann@ucar.edu>
2019-03-13 12:47:53 -0700

    Modify default var request and catch exceptions that occur

--- scripts/download_icar.py
+++ scripts/download_icar.py
@@ -4,6 +4,7 @@ from download import get_dataset
 
 # If it is possible to get all of the MPI data (with divergence / vorticity winds) these can be converted
 #  to u and v winds using something like "cdo -s -r -f nc setreftime,1860-01-01,00:00 -settunits,day -sp2gp -dv2uv input.grb output.nc"
+#
 #  to remap a rotated pole (e.g. ocean temperatures) first create a cdo grid description:
 #   cdo griddes sample_file.nc  > grid.txt
 #  then remap to it
@@ -13,7 +14,7 @@ from download import get_dataset
 core_variables = [{"var_name":"va", "domain":"atmos", "interval":"6hr"},
                   {"var_name":"ua", "domain":"atmos", "interval":"6hr"},
                   {"var_name":"ta", "domain":"atmos", "interval":"6hr"},
-                  {"var_name":"ps", "domain":"atmos", "interval":"6hr"},
+                  # {"var_name":"ps", "domain":"atmos", "interval":"6hr"},
                   {"var_name":"hus", "domain":"atmos", "interval":"6hr"},
                   {"var_name":"prc", "domain":"atmos", "interval":"3hr"},
                   {"var_name":"tos", "domain":"ocean", "interval":"day"},
@@ -22,17 +23,17 @@ core_variables = [{"var_name":"va", "domain":"atmos", "interval":"6hr"},
                   ]
 
 # daily, low time resolution download
-core_variables = [{"var_name":"va", "domain":"atmos", "interval":"day"},
-                  {"var_name":"ua", "domain":"atmos", "interval":"day"},
-                  {"var_name":"ta", "domain":"atmos", "interval":"day"},
-                  {"var_name":"ps", "domain":"atmos", "interval":"day"},
-                  {"var_name":"hus", "domain":"atmos", "interval":"day"},
-                  {"var_name":"zg", "domain":"atmos", "interval":"day"},
-                  {"var_name":"prc", "domain":"atmos", "interval":"day"},
-                  {"var_name":"tos", "domain":"ocean", "interval":"day"},
-                  {"var_name":"orog", "domain":"atmos", "interval":"fx"},
-                  {"var_name":"sftlf", "domain":"atmos", "interval":"fx"},
-                  ]
+# core_variables = [{"var_name":"va", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"ua", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"ta", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"ps", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"hus", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"zg", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"prc", "domain":"atmos", "interval":"day"},
+#                   {"var_name":"tos", "domain":"ocean", "interval":"day"},
+#                   {"var_name":"orog", "domain":"atmos", "interval":"fx"},
+#                   {"var_name":"sftlf", "domain":"atmos", "interval":"fx"},
+#                   ]
 
 
 
@@ -40,7 +41,12 @@ def main(model="CCSM4", run="r6i1p1", scenario="historical", start_date="1980010
 
     for v in core_variables:
         print(v)
-        get_dataset.download(model=model, run=run, scenario=scenario, start_time=start_date, end_time=end_date, **v)
+        try:
+            get_dataset.download(model=model, run=run, scenario=scenario, start_time=start_date, end_time=end_date, **v)
+        except Exception as e:
+            print("Error: ")
+            print(e)
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main(model="CanESM2", run="r1i1p1", scenario="historical")
